aa6d08b7a8b64fb5c4f0f49c03c6f78a2b7a2d2d
Jan van der Lugt <janlugt@gmail.com>
2017-07-25 09:37:30 +0200

    second commit

--- bikelink2kml.py
+++ bikelink2kml.py
@@ -1,4 +1,5 @@
 import ast
+import fileinput
 import re
 import simplekml
 import urllib2
@@ -17,7 +18,36 @@ for str in location_strings:
   locations.append(ast.literal_eval(dict))
 
 kml = simplekml.Kml()
-for l in locations:
-  kml.newpoint(name=l['street_address'], coords=[(l['longitude'], l['latitude'])])
-  
-kml.save('bikelink.kml')
+elocker_style = simplekml.Style()
+elocker_style.iconstyle.icon.href = 'https://www.bikelink.org/assets/lockers-c9deb0ef775179becfff02261c117aa4.png'
+group_style = simplekml.Style()
+group_style.iconstyle.icon.href = 'https://www.bikelink.org/assets/group-5637fbe44207d3fe6b92d82bc39a24bc.png'
+
+for loc in locations:
+  # Ignore vendor locations
+  loc_type = loc['location_friendly_type']
+  if loc_type == 'Vendor':
+    continue
+
+  pnt = kml.newpoint(name='<![CDATA[%s]]>' % loc['human_name'])
+  pnt.coords = [(loc['longitude'], loc['latitude'])]
+  pnt.address = '%s, %s %s' % (loc['street_address'], loc['state_abbreviation'], loc['postal_code'])
+  if loc_type == 'eLocker':
+    pnt.style = elocker_style
+  elif loc_type == 'Group Parking':
+    pnt.style = group_style
+
+kml.save('bikelink_child.kml')
+
+# Super hacky, because simplekml seems unable to export ampersands
+with open('bikelink_child.kml', 'r') as file:
+  filedata = file.read()
+filedata = filedata.replace('\u0026', '&')
+with open('bikelink_child.kml', 'w') as file:
+  file.write(filedata)
+
+kml2 = simplekml.Kml()
+netlink = kml2.newnetworklink(name="Network Link")
+netlink.link.href = "https://raw.githubusercontent.com/janlugt/bikelink2kml/master/bikelink_child.kml"
+netlink.link.viewrefreshmode = simplekml.ViewRefreshMode.onrequest
+kml.save("bikelink_parent.kml")
